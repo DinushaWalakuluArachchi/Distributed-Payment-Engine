70c6a5e206fe840111d7ff78d66e12d72224e003
DinushKmadhuwantha <dinumadhu22@gmail.com>
2026-06-27 18:36:53 +0530

    updated

--- account-service/src/main/java/com/paymentengine/accountservice/repository/DebitRecordRepository.java
+++ account-service/src/main/java/com/paymentengine/accountservice/repository/DebitRecordRepository.java
@@ -3,10 +3,12 @@ package com.paymentengine.accountservice.repository;
 import com.paymentengine.accountservice.domain.DebitRecord;
 import com.paymentengine.accountservice.domain.DebitStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface DebitRecordRepository extends JpaRepository<DebitRecord, UUID> {
 
     Optional<DebitRecord> findByPaymentIdAndStatus(
