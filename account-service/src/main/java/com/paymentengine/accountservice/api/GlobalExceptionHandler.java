5d8a7344664bc9cef68f3f93ce418a9d3282f0a3
DinushKmadhuwantha <dinumadhu22@gmail.com>
2026-06-27 18:37:04 +0530

    updated

--- account-service/src/main/java/com/paymentengine/accountservice/api/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/paymentengine/accountservice/api/GlobalExceptionHandler.java
@@ -1,4 +1,29 @@
 package com.paymentengine.accountservice.api;
 
+import com.paymentengine.accountservice.domain.AccountNotFoundException;
+import com.paymentengine.accountservice.domain.InsufficientFundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InsufficientFundException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ProblemDetail handleInsufficientFunds(InsufficientFundException ex){
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage());
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ProblemDetail handleNotFound(AccountNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+
 }
