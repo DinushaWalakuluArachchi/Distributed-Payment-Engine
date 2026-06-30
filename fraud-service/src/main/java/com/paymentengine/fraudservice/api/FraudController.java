62fce8f501e9bc1adffdcf7f61dda128769556e5
DinushKmadhuwantha <dinumadhu22@gmail.com>
2026-06-30 08:26:03 +0530

    update

--- fraud-service/src/main/java/com/paymentengine/fraudservice/api/FraudController.java
+++ fraud-service/src/main/java/com/paymentengine/fraudservice/api/FraudController.java
@@ -1,4 +1,46 @@
 package com.paymentengine.fraudservice.api;
 
+import com.paymentengine.fraudservice.service.FraudScoringService;
+import com.paymentengine.fraudservice.service.FraudSignalCheck;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/fraud")
+@RequiredArgsConstructor
 public class FraudController {
+
+    private final FraudScoringService fraudScoringService;
+
+    @PostMapping("/check")
+    public ResponseEntity<FraudScoringService.FraudCheckResult> check(@RequestBody FraudCheckApiRequest request){
+
+        var result = fraudScoringService.evaluate(
+                new FraudSignalCheck.FraudCheckRequest(
+                        request.paymentId,
+                        request.senderId,
+                        request.receiverId,
+                        request.amount,
+                        request.currency
+                )
+        );
+
+        return ResponseEntity.ok(result);
+    }
+
+    record FraudCheckApiRequest(
+            UUID paymentId, UUID senderId, UUID receiverId,
+            BigDecimal amount, String currency
+    ){}
+
+
+
+
 }
