c18788265f432911b3868ec7e1a6b5497b4dc354
DinushKmadhuwantha <dinumadhu22@gmail.com>
2026-07-01 02:08:18 +0530

    create

--- fraud-service/src/main/java/com/paymentengine/fraudservice/service/FraudSignalCheck.java
+++ fraud-service/src/main/java/com/paymentengine/fraudservice/service/FraudSignalCheck.java
@@ -1,4 +1,21 @@
 package com.paymentengine.fraudservice.service;
 
-public class FraudSignalCheck {
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public interface FraudSignalCheck {
+
+    String getSignalType();
+
+    BigDecimal evaluate(FraudCheckRequest request);
+
+    record FraudCheckRequest(
+            UUID paymentId,
+            UUID senderId,
+            UUID receiverId,
+            BigDecimal amount,
+            String currency
+    ){
+
+    }
 }
