ad387191c558a17f6722e8d155622562ebca0ffe
DinushKmadhuwantha <dinumadhu22@gmail.com>
2026-07-04 21:40:26 +0530

    unversion

---
+++ payment-service/src/test/java/com/paymentengine/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentengine.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shared/src/test/java/com/paymentengine/shared/SharedApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.paymentengine.shared;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SharedApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
