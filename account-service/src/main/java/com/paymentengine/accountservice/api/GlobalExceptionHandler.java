6ebb7cd2c46c983e4012a12a478b524e850fc40d
DinushKmadhuwantha <dinumadhu22@gmail.com>
2026-07-04 13:31:21 +0530

    add metrics part to show performance in grafana

--- account-service/src/main/java/com/paymentengine/accountservice/api/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/paymentengine/accountservice/api/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.paymentengine.accountservice.api;
 
 import com.paymentengine.accountservice.domain.AccountNotFoundException;
 import com.paymentengine.accountservice.domain.InsufficientFundException;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MeterRegistry meterRegistry;
 
     @ExceptionHandler(InsufficientFundException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public ProblemDetail handleInsufficientFunds(InsufficientFundException ex){
+        meterRegistry.counter("account.debits.insufficient_funds").increment();
         return ProblemDetail.forStatusAndDetail(
                 HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage());
     }
